250acee2a118a1ec4849a6d02a4a1792dd3b895b
Tushar Ahmmed Sakib <tusharahmmedsakib@gmail.com>
2023-07-08 01:26:54 +0600

    setup eslint pretter husky lint-stage

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function connectDatabase() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
       console.log(
-        `University management auth service listening on port ${config.port}`
+        `University management auth service listening on port ${config.port}`,
       );
     });
   } catch (err) {
-    console.log(err, "Fail to connect Database");
+    console.log(err, 'Fail to connect Database');
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
